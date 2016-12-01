8c4f86fed2beed02d222240f916efe9ec4688cb1
lili <liq1862@gmail.com>
2016-12-01 11:03:39 +0800

    點Button重新讀取資料

--- app/src/main/java/com/example/user/a20161201_test1/MainActivity.java
+++ app/src/main/java/com/example/user/a20161201_test1/MainActivity.java
@@ -14,6 +14,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
@@ -52,4 +60,55 @@ public class MainActivity extends AppCompatActivity {
         queue.start();
 
     }
+//  ========================================================================
+    /*點Button重新讀取資料*/
+    public void click2(View v)
+    {
+        GetData t = new GetData();
+        t.start();
+    }
+
+    private class GetData extends Thread {
+        InputStream inputStream = null;
+        @Override
+        public void run()
+        {
+            URL url = null;
+            try {
+                url = new URL("https://raspberrytemp-abf6c.firebaseio.com/temp.json");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                inputStream = conn.getInputStream();
+
+                //==============
+                ByteArrayOutputStream result = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    result.write(buffer, 0, length);
+                }
+                String str = result.toString();
+                JSONObject obj = new JSONObject(str);
+                final double temp = obj.getDouble("temp");
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(String.valueOf(temp));
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+//  =============================================================================
 }
